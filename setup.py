c362018fc705aa7ab37b937a1dd8bf0069877a5f
Gustav <gustav@timeanddate.com>
2023-02-07 10:55:05 +0100

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
 setup(name='libtad',
-      version='1.1',
+      version='1.1.1',
       description='Python library for accessing Time and Date APIs',
       author='Time and Date',
       url='https://github.com/timeanddate/libtad-python/',
